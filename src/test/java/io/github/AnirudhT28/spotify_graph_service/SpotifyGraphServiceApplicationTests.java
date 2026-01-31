df2bad0bb3e121f2195eae808b71bafdda4ae0f0
Anirudh Tiwari <anirudhtiwari2805@gmail.com>
2026-01-31 20:31:04 +0100

    first commit

---
+++ src/test/java/io/github/AnirudhT28/spotify_graph_service/SpotifyGraphServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.AnirudhT28.spotify_graph_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyGraphServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
