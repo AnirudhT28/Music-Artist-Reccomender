eb1220ba3af904e5fe0fea19a330986504c271fd
Anirudh Tiwari <anirudhtiwari2805@gmail.com>
2026-02-01 00:52:50 +0100

    fix link

---
+++ src/test/java/io/github/AnirudhT28/spotify_graph_service/SpotifyGraphServiceApplication.java
@@ -0,0 +1,12 @@
+package io.github.AnirudhT28.spotify_graph_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyGraphServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyGraphServiceApplication.class, args);
+    }
+}
