cb56f75444a11d0bbeaee4e7a5bd81ee68e378f7
Anirudh Tiwari <anirudhtiwari2805@gmail.com>
2026-02-02 15:21:51 +0100

    create endpoints for artist names

--- src/main/java/io/github/AnirudhT28/spotify_graph_service/controller/ArtistController.java
+++ src/main/java/io/github/AnirudhT28/spotify_graph_service/controller/ArtistController.java
@@ -2,9 +2,7 @@ package io.github.AnirudhT28.spotify_graph_service.controller;
 
 import io.github.AnirudhT28.spotify_graph_service.model.Artist;
 import io.github.AnirudhT28.spotify_graph_service.repository.ArtistRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -17,14 +15,16 @@ public class ArtistController {
         this.repository = repository;
     }
 
-    // This is our sanity check URL
-    @GetMapping("/test")
-    public String test() {
-        return "The API is alive and reachable!";
+    @GetMapping("/names")
+    public List<String> getNames() {
+        return repository.findAllArtistNames();
     }
 
     @GetMapping("/gems")
-    public List<Artist> getHiddenGems() {
+    public List<Artist> getGems(@RequestParam(required = false) String seed) {
+        if (seed != null && !seed.isEmpty()) {
+            return repository.findGemsBySeed(seed);
+        }
         return repository.findHiddenGems();
     }
 }
