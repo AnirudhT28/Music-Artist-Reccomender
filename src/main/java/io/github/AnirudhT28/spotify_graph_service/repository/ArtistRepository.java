4db2ea5d2c38cc35f6e2045244602f15fafb32de
Anirudh Tiwari <anirudhtiwari2805@gmail.com>
2026-02-02 15:21:09 +0100

    get artist names

--- src/main/java/io/github/AnirudhT28/spotify_graph_service/repository/ArtistRepository.java
+++ src/main/java/io/github/AnirudhT28/spotify_graph_service/repository/ArtistRepository.java
@@ -1,16 +1,27 @@
-package io.github.AnirudhT28.spotify_graph_service.repository; 
+package io.github.AnirudhT28.spotify_graph_service.repository;
 
-import io.github.AnirudhT28.spotify_graph_service.model.Artist; 
+import io.github.AnirudhT28.spotify_graph_service.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ArtistRepository extends JpaRepository<Artist, String> {
-    
-    /**
-     * The "Hidden Gem" Algorithm: Influence / (Listeners + 1)
-     * nativeQuery = true tells Spring to use standard SQL for SQLite.
-     */
+
+    // Global Hidden Gems
     @Query(value = "SELECT * FROM artists ORDER BY score / (1.0 + listeners) DESC LIMIT 10", nativeQuery = true)
     List<Artist> findHiddenGems();
+
+    // Get all names for the search dropdown
+    @Query(value = "SELECT name FROM artists ORDER BY name ASC", nativeQuery = true)
+    List<String> findAllArtistNames();
+
+    
+    // PERSONALIZED QUERY: Finds neighbors of the seed artist 
+     
+    @Query(value = "SELECT a.* FROM artists a " +
+                   "JOIN influences i ON a.name = i.influenced_name " +
+                   "WHERE i.artist_name = :seedName " +
+                   "ORDER BY a.score / (1.0 + a.listeners) DESC LIMIT 10", nativeQuery = true)
+    List<Artist> findGemsBySeed(@Param("seedName") String seedName);
 }
